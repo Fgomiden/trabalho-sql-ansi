bf8b727b6c5a6a9dfd104ebe65efc3766c597521
Fgomiden <51092409+Fgomiden@users.noreply.github.com>
2020-11-14 18:01:33 -0300

    Update ansi_vs_oracle.sql

--- ansi_vs_oracle.sql
+++ ansi_vs_oracle.sql
@@ -1,47 +1,106 @@
---Comparação ANSI x Dialeto Oracle
+--Comparação SQL89 x SQL92
 
--- LEFT JOIN
+--INNER JOIN
 
---Oracle
-SELECT 
-    alunos.nome as Aluno, 
-    aulas.nome as Aula,
-    professor as Professor
-FROM aulas, alunos
-WHERE aulas.id = alunos.aula_id; --(+) pesquisar simbolo depois
-
--- ANSI
-
-SELECT 
-    alunos.nome as Aluno, 
-    aulas.nome as Aula,
-    professor as Professor
-FROM  aulas
-LEFT JOIN alunos ON aulas.id = alunos.aula_id;
-
--- FULL OUTER JOIN
-
---Oracle
- SELECT 
-    alunos.nome as Aluno, 
-    aulas.nome as Aula,
-    professor as Professor
-FROM aulas, alunos
-WHERE aulas.id = alunos.aula_id;
-UNION
+--SQL89
 SELECT 
     alunos.nome as Aluno, 
     aulas.nome as Aula,
     professor as Professor
 FROM aulas, alunos
 WHERE aulas.id = alunos.aula_id;
+  --  AND alunos.id >= 3;
 
--- ANSI
+--SQL92
 SELECT 
     alunos.nome as Aluno, 
     aulas.nome as Aula,
     professor as Professor
 FROM  aulas
-FULL OUTER JOIN alunos ON aulas.id = alunos.aula_id; --MySQL nao suporta 
+INNER JOIN alunos 
+    ON aulas.id = alunos.aula_id;
+--WHERE alunos.id = 1;
 
 
+--LEFT JOIN
+
+--SQL89
+--com o (+) não roda mas era assim antigamente
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM aulas, alunos
+WHERE aulas.id(+) = alunos.aula_id; 
+ --   AND alunos.id = 1;
+
+--SQL92
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM  aulas
+    LEFT JOIN alunos 
+        ON aulas.id = alunos.aula_id;
+--WHERE alunos.id = 1;
+
+
+--RIGHT JOIN
+
+--SQL89
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM aulas, alunos
+WHERE aulas.id = alunos.aula_id(+);
+ --   AND alunos.id = 1;
+
+--SQL92
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM  aulas
+    RIGHT JOIN alunos 
+        ON aulas.id = alunos.aula_id;
+--WHERE alunos.id = 1;
+
+
+--No SQL 89 colacava o (+) do lado esquerdo ou no direito
+
+
+--FULL JOIN
+
+--SQL89 não tem suporte ao FULL JOIN
+
+
+--SQL92
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM  aulas
+    FULL JOIN alunos -- Não funciona no MySQL
+        ON aulas.id = alunos.aula_id;
+--WHERE alunos.id = 1;
+
+
+--CROSS JOIN
+
+--SQL89 
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM aulas, alunos
+
+--SQL92
+SELECT 
+    alunos.nome as Aluno, 
+    aulas.nome as Aula,
+    professor as Professor
+FROM  aulas
+    CROSS JOIN alunos 
+        ON aulas.id = alunos.aula_id;
+--WHERE alunos.id = 1;
